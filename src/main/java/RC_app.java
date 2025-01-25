98d1fed8126205c8fad66d62c82bad476ce06653
rchlai <22b6034@ubd.edu.bn>
2025-01-25 13:53:02 +0800

    Added echo function
    Also changed the chatbot personality to be polite and command formats

--- src/main/java/RC_app.java
+++ src/main/java/RC_app.java
@@ -1,11 +1,23 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import org.w3c.dom.ls.LSInput;
 
 public class RC_app {
     public static void greet() {
-        System.out.println("Hello! I'm RC");
-        System.out.println("What can I do for you?");
-        System.out.println("\n");
-        System.out.println("Bye. Hope to see you again soon!");;
+        Scanner input = new Scanner(System.in);
+        String line;
+
+        System.out.println("Good day! I'm RC, your personal chatbot.");
+        System.out.println("You need my assistance today?\n");
+        
+        do {
+            System.out.print("User says: ");
+            line = input.nextLine();
+            System.out.println("RC says: " + line + "\n");
+        } while((!line.equals("bye")));
+
+        System.out.println("Goodbye. Hope I satisfy your needs for today!");;
     }
 
     public static void main(String[] args) {
