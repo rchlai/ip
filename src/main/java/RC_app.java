7d0ca0a8cdc7bdd229ce053638869c9f07b3807c
rchlai <22b6034@ubd.edu.bn>
2025-01-25 15:05:59 +0800

    Added add and list function
    Also change greet() to chatWithBot() and change from using do-while statement to while statement for simplicity

--- src/main/java/RC_app.java
+++ src/main/java/RC_app.java
@@ -1,21 +1,35 @@
+import java.sql.Array;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 import org.w3c.dom.ls.LSInput;
 
 public class RC_app {
-    public static void greet() {
+    public static void chatWithBot() {
         Scanner input = new Scanner(System.in);
         String line;
+        String[] texts = new String[100];
+        int textCount = 0;
 
         System.out.println("Good day! I'm RC, your personal chatbot.");
         System.out.println("You need my assistance today?\n");
-        
-        do {
+
+        System.out.print("User says: ");
+        line = input.nextLine();
+        while(!line.equals("bye")) {
+            if(line.equals("list")) {
+                for(int i = 0; i < texts.length; i++) {
+                    System.out.println((i + 1) + ". " + texts[i]);
+                }
+            } else {
+                texts[textCount] = line;
+                textCount++;
+                System.out.println("RC added: " + line + "\n");
+            }
+
             System.out.print("User says: ");
             line = input.nextLine();
-            System.out.println("RC says: " + line + "\n");
-        } while((!line.equals("bye")));
+        };
 
         System.out.println("Goodbye. Hope I satisfy your needs for today!");;
     }
@@ -29,6 +43,6 @@ public class RC_app {
         +"|__|__/\\______/\n";
 
         System.out.println("Hello from\n" + logo);
-        greet();
+        chatWithBot();
     }
 }
