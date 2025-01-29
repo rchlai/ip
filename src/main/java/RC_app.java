0f750ac1d8dae447f7dc8b5fbe6ca90515b16883
rchlai <22b6034@ubd.edu.bn>
2025-01-29 09:54:03 +0800

    Coded 'Mark as Done' using Task class
    Also commented out some methods in Task class worth coding for future implementations

--- src/main/java/RC_app.java
+++ src/main/java/RC_app.java
@@ -8,26 +8,36 @@ public class RC_app {
     public static void chatWithBot() {
         Scanner input = new Scanner(System.in);
         String line;
-        String[] texts = new String[100];
-        int textCount = 0;
+        int markIndex;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         System.out.println("Good day! I'm RC, your personal chatbot.");
         System.out.println("You need my assistance today?\n");
 
         System.out.print("User says: ");
         line = input.nextLine();
+
         while(!line.equals("bye")) {
             if(line.equals("list")) {
-                for(int i = 0; i < texts.length; i++) {
-                    System.out.println((i + 1) + ". " + texts[i]);
+                System.out.println("Here are the tasks in your list:");
+                for(int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription()); // displays as 1.[X] read book and so on
                 }
+                System.out.println(); // create an empty line to avoid text clutter
+            } else if(line.contains("mark")) {
+                markIndex = Integer.parseInt(line.replaceAll("[^0-9]", "")); // extracts digits from string to be converted into integer type
+                tasks[markIndex - 1].markAsDone(); // use markIndex to mark selected task from Task[] as done
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(markIndex + "." + "[" + tasks[markIndex - 1].getStatusIcon() + "] " + tasks[markIndex - 1].getDescription() + "\n"); // displays marked task
             } else {
-                texts[textCount] = line;
-                textCount++;
-                System.out.println("RC added: " + line + "\n");
+                Task t = new Task(line); // create a new Task instance
+                System.out.println("RC added: " + t.getDescription() + "\n");
+                tasks[taskCount] = t; // add Task instance into Task[]
+                taskCount++;
             }
 
-            System.out.print("User says: ");
+            System.out.print("User says: "); // prompt user to write next command
             line = input.nextLine();
         };
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,41 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+//    public void addTask() {
+//        this.texts[textCount] = this.description;
+//        textCount++;
+//    }
+//
+//    public void listTask() {
+//        System.out.println("RC added: " + getDescription() + "\n");
+//    }
+//
+//    public void listAllTasks() {
+//        System.out.println("Here are the tasks in your list:");
+//        for(int i = 0; i < textCount; i++) {
+//            System.out.println((i + 1) + "." + "[" + getStatusIcon() + "] " + texts[i]); // displays as 1.[X] read book and so on
+//        }
+//    }
 }
