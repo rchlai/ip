cadc8c62bb212484561109c9bb31547d150163c8
rchlai <22b6034@ubd.edu.bn>
2025-02-05 09:06:56 +0800

    Refactored Code to Improve Code Quality

--- src/main/java/RCApp.java
+++ src/main/java/RCApp.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 
 public class RCApp {
+    // global variables
     static Scanner input = new Scanner(System.in);
     static Task[] tasks = new Task[100];
     static int taskCount = 0;
 
+    // global prefix constants
+    static final String BY_PREFIX = "/by";
+    static final String FROM_PREFIX = "/from";
+    static final String TO_PREFIX = "/to";
+    static final String TO_DO_PREFIX = "todo";
+    static final String DEADLINE_PREFIX = "deadline";
+    static final String EVENT_PREFIX = "event";
+
     public static void chatWithBot() {
-        // prefix constants
-        final String BY_PREFIX = "/by";
-        final String FROM_PREFIX = "/from";
-        final String TO_PREFIX = "/to";
-        final String TO_DO_PREFIX = "todo";
-        final String DEADLINE_PREFIX = "deadline";
-        final String EVENT_PREFIX = "event";
-
-        String description;
-        String dueDate;
-        String start;
-        String end;
-        int indexOfByPrefix;
-        int indexOfFromPrefix;
-        int indexOfToPrefix;
-
+        printChatbotLogo();
         printWelcomeMessage();
 
         while (true) {
@@ -30,9 +24,10 @@ public class RCApp {
             String line = input.nextLine();
 
             if (line.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
                 printAllTasks();
+                // print a line separator to avoid text clutter
                 addLineSeparator();
+                // skip to the next iteration
                 continue;
             }
 
@@ -49,41 +44,13 @@ public class RCApp {
             }
             
             if (line.equals("bye")) {
+                // exit the while loop
                 break;
             }
 
-            if (line.startsWith(TO_DO_PREFIX)) {
-                description = line.replace(TO_DO_PREFIX, "").trim();
-
-                ToDo td = new ToDo(description);
-                addTask(td);
-                System.out.println("This task has been added: " +  "\n" + td);
-            }
-
-            if (line.startsWith(DEADLINE_PREFIX)) {
-                indexOfByPrefix = line.indexOf(BY_PREFIX);
-
-                description = line.substring(0, indexOfByPrefix).replace(DEADLINE_PREFIX, "").trim();
-                dueDate = line.substring(indexOfByPrefix).replace("/by", "").trim();
-
-                Deadline d = new Deadline(description, dueDate);
-                addTask(d);
-                System.out.println("This task has been added: " +  "\n" + d);
-            }
-
-            if (line.startsWith(EVENT_PREFIX)) {
-                indexOfFromPrefix = line.indexOf("/from");
-                indexOfToPrefix = line.indexOf("/to");
-
-                description = line.substring(0, indexOfFromPrefix).replace(EVENT_PREFIX, "").trim();
-                start = line.substring(indexOfFromPrefix, indexOfToPrefix).replace(FROM_PREFIX, "").trim();
-                end = line.substring(indexOfToPrefix).replace(TO_PREFIX, "").trim();
-
-                Event e = new Event(description, start, end);
-                addTask(e);
-                System.out.println("This task has been added: " +  "\n" + e);
-            }
-
+            // check Task type (i.e., to do, event, deadline)
+            determineTaskType(line);
+            // print number of tasks added
             displayNumOfTasks();
             addLineSeparator();
         };
@@ -97,15 +64,6 @@ public class RCApp {
         System.out.println("You need my assistance today?\n");
     }
 
-    public static void printFarewellMessage() {
-        System.out.println("Goodbye. Hope I satisfy your needs for today!");
-    }
-
-    public static void addLineSeparator() {
-        // print a line separator to avoid text clutter
-        System.out.println("============================================");
-    }
-
     public static void printChatbotLogo() {
         String logo =
                 "______________\n"
@@ -117,43 +75,133 @@ public class RCApp {
         System.out.println("Hello from\n" + logo);
     }
 
+    public static void printFarewellMessage() {
+        System.out.println("Goodbye. Hope I satisfy your needs for today!");
+    }
+
+    public static void addLineSeparator() {
+        System.out.println("============================================");
+    }
+
     public static void addTask(Task t) {
         tasks[taskCount] = t;
         taskCount++;
+        System.out.println("This task has been added: " +  "\n" + t);
     }
 
     public static void displayNumOfTasks() {
-        System.out.println("You have " + taskCount + " task(s) in the list");
+        System.out.println("You now have " + taskCount + " task(s) in the list");
     }
 
     public static void printAllTasks() {
+        System.out.println("Here are the tasks in your list:");
+
         for (int i = 0; i < taskCount; i++) {
             // displays as 1.[X] <description> and so on
             System.out.println((i + 1) + "." + tasks[i]);
         }
     }
 
-    private static void unmarkTask(String line) {
-        int unmarkIndex = Integer.parseInt(line.replaceAll("[^0-9]", ""));
-        // use unmarkIndex to mark task from Task[] as not done
-        tasks[unmarkIndex - 1].markAsNotDone();
-        System.out.println("Noted, I've marked this task as not done yet:");
-        // displays unmarked task
-        System.out.println(unmarkIndex + "." + tasks[unmarkIndex - 1]);
-    }
-
     private static void markTask(String line) {
-        // extracts digits from string to be converted into integer type
+        // extract digits from string to be converted into integer type
         int markIndex = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+
         // use markIndex to mark task from Task[] as done
         tasks[markIndex - 1].markAsDone();
+
         System.out.println("Good job! I'll mark this task as done:");
-        // displays marked task
+        // display marked task
         System.out.println(markIndex + "." + tasks[markIndex - 1]);
     }
 
+    private static void unmarkTask(String line) {
+        int unmarkIndex = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+
+        // use unmarkIndex to mark task from Task[] as not done
+        tasks[unmarkIndex - 1].markAsNotDone();
+
+        System.out.println("Noted, I've marked this task as not done yet:");
+        // display unmarked task
+        System.out.println(unmarkIndex + "." + tasks[unmarkIndex - 1]);
+    }
+
+    private static void determineTaskType(String line) {
+        String taskType = extractTaskType(line);
+
+        switch (taskType) {
+        case "todo":
+            handleToDo(line);
+            break;
+        case "deadline":
+            handleDeadline(line);
+            break;
+        case "event":
+            handleEvent(line);
+            break;
+        default:
+            System.out.println("Invalid task format.");
+            break;
+        }
+    }
+
+    private static String extractTaskType(String line) {
+        if (line.startsWith(TO_DO_PREFIX)) {
+            return "todo";
+        }
+
+        if (line.startsWith(DEADLINE_PREFIX)) {
+            return "deadline";
+        }
+
+        if (line.startsWith(EVENT_PREFIX)) {
+            return "event";
+        }
+
+        return "invalid";
+    }
+
+    private static void handleToDo(String line) {
+        // remove "to-do" prefix from line and any leading & trailing whitespaces
+        String description = line.replace(TO_DO_PREFIX, "").trim();
+
+        // create a new to-do instance
+        ToDo td = new ToDo(description);
+        addTask(td);
+    }
+
+    private static void handleDeadline(String line) {
+        // obtain index of "/by" from line
+        int indexOfByPrefix = line.indexOf(BY_PREFIX);
+
+        // extract "deadline <description>" from line and remove "deadline" prefix
+        String description = line.substring(0, indexOfByPrefix).replace(DEADLINE_PREFIX, "").trim();
+        // extract "/by <due date>" from line and remove "/by" prefix
+        String dueDate = line.substring(indexOfByPrefix).replace(BY_PREFIX, "").trim();
+
+        // create a new Deadline instance
+        Deadline d = new Deadline(description, dueDate);
+        addTask(d);
+    }
+
+    private static void handleEvent(String line) {
+        // obtain index of "/from" from line
+        int indexOfFromPrefix = line.indexOf(FROM_PREFIX);
+        // obtain index of "/to" from line
+        int indexOfToPrefix = line.indexOf(TO_PREFIX);
+
+        // extract "event <description>" from line and remove "event" prefix
+        String description = line.substring(0, indexOfFromPrefix).replace(EVENT_PREFIX, "").trim();
+        // extract "/from <start>" from line and remove "/from" prefix
+        String start = line.substring(indexOfFromPrefix, indexOfToPrefix).replace(FROM_PREFIX, "").trim();
+        // extract "/to <end>" from line and remove "/to" prefix
+        String end = line.substring(indexOfToPrefix).replace(TO_PREFIX, "").trim();
+
+        // create a new Event instance
+        Event e = new Event(description, start, end);
+        addTask(e);
+    }
+
     public static void main(String[] args) {
-        printChatbotLogo();
         chatWithBot();
     }
 }
