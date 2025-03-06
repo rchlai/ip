f559af1a2252bd5ffd3838b254d70ab042368e75
rchlai <22b6034@ubd.edu.bn>
2025-03-06 15:29:15 +0800

    Added JavaDocs to Deadline subclass

--- src/main/java/rc/task/Deadline.java
+++ src/main/java/rc/task/Deadline.java
@@ -1,26 +1,68 @@
 package rc.task;
 
+/**
+ * Represents a `Deadline` subclass inheriting the properties of the task
+ * superclass.
+ * <p>
+ * Identified by the capital letter D.
+ */
 public class Deadline extends Task {
     protected String by;
 
+    /**
+     * Constructs a `Deadline` task with the specified description and
+     * due date.
+     *
+     * @param description The description of the `Deadline` task.
+     * @param due The due date of the task
+     */
     public Deadline(String description, String due) {
         super(description);
         setBy(due);
     }
 
+    /**
+     * Returns the deadline due date.
+     *
+     * @return due date
+     */
     public String getBy() {
         return by;
     }
 
+    /**
+     * Sets the deadline due date.
+     *
+     * @param due Due date of the deadline task
+     */
     public void setBy(String due) {
         by = due;
     }
 
+    /**
+     * Returns a string representation of the `Deadline` task.
+     * <p>
+     * The format includes the task type `[D]` followed by the task's status,
+     * description, and due date (e.g., `[D][X] Submit work (by: Monday 2pm)`).
+     * </p>
+     *
+     * @return A string representation of the `Deadline` task.
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + getBy() + ")";
     }
 
+    /**
+     * Returns a formatted string representation of the `Deadline` task when
+     * saving it to a file.
+     * <p>
+     * The format includes the task type `D` followed by the task's status,
+     * description, and due date (e.g., `D | 1 | Buy groceries | Monday 2pm`).
+     * </p>
+     *
+     * @return A string representation of the `Deadline` task in file format.
+     */
     @Override
     public String toFileFormat() {
         return "D" + super.toFileFormat() + " | " + getBy();
